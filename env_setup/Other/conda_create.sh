3c310c9fcf11acebfb60b6560f422c7fcede5eab
Xiner Zhao <xiner_zhao@brown.edu>
2024-05-03 16:22:28 -0400

    Delete env_setup directory

--- env_setup/Other/conda_create.sh
+++
@@ -1,5 +0,0 @@
-conda env remove -n "ChineseToEnglish"
-conda env create -n "ChineseToEnglish" -f env_setup/Other/ChineseToEnglish.yml
-
-## Install new environment.
-python3 -m ipykernel install --user --name ChineseToEnglish --display-name "DL-S24 (3.10)"
