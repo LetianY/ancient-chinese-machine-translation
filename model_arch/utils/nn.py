4b4ca8819ab70b3c54e843c36a2f63ae579eeef6
LetianY <DawnEureka@outlook.com>
2024-05-04 15:43:23 -0400

    Revise file structure

--- model_arch/utils/nn.py
+++ model_arch/utils/nn.py
@@ -7,7 +7,6 @@ import math
 import torch as th
 import torch.nn as nn
 
-
 # PyTorch 1.7 has SiLU, but we support PyTorch 1.5.
 class SiLU(nn.Module):
     def forward(self, x):
@@ -17,20 +16,6 @@ class SiLU(nn.Module):
 class GroupNorm32(nn.GroupNorm):
     def forward(self, x):
         return super().forward(x.float()).type(x.dtype)
-
-
-def conv_nd(dims, *args, **kwargs):
-    """
-    Create a 1D, 2D, or 3D convolution module.
-    """
-    if dims == 1:
-        return nn.Conv1d(*args, **kwargs)
-    elif dims == 2:
-        return nn.Conv2d(*args, **kwargs)
-    elif dims == 3:
-        return nn.Conv3d(*args, **kwargs)
-    raise ValueError(f"unsupported dimensions: {dims}")
-
 
 def linear(*args, **kwargs):
     """
@@ -83,19 +68,11 @@ def scale_module(module, scale):
     return module
 
 
-def mean_flat(tensor, mask = None):
+def mean_flat(tensor):
     """
     Take the mean over all non-batch dimensions.
     """
-    if mask is None:
-        return tensor.mean(dim=list(range(1, len(tensor.shape))))
-    else:
-        for _ in range(len(tensor.shape)-len(mask.shape)):
-            mask = mask.unsqueeze(-1)
-        mask = mask.expand(tensor.shape)
-        tensor = tensor * mask
-        return tensor.sum(dim=list(range(1, len(tensor.shape)))) / mask.sum(dim=list(range(1, len(mask.shape))))
-
+    return tensor.mean(dim=list(range(1, len(tensor.shape))))
 
 
 def normalization(channels):
@@ -127,51 +104,3 @@ def timestep_embedding(timesteps, dim, max_period=10000):
     if dim % 2:
         embedding = th.cat([embedding, th.zeros_like(embedding[:, :1])], dim=-1)
     return embedding
-
-def checkpoint(func, inputs, params, flag):
-    """
-    Evaluate a function without caching intermediate activations, allowing for
-    reduced memory at the expense of extra compute in the backward pass.
-
-    :param func: the function to evaluate.
-    :param inputs: the argument sequence to pass to `func`.
-    :param params: a sequence of parameters `func` depends on but does not
-                   explicitly take as arguments.
-    :param flag: if False, disable gradient checkpointing.
-    """
-    if flag:
-        args = tuple(inputs) + tuple(params)
-        return CheckpointFunction.apply(func, len(inputs), *args)
-    else:
-        return func(*inputs)
-
-
-class CheckpointFunction(th.autograd.Function):
-    @staticmethod
-    def forward(ctx, run_function, length, *args):
-        ctx.run_function = run_function
-        ctx.input_tensors = list(args[:length])
-        ctx.input_params = list(args[length:])
-        with th.no_grad():
-            output_tensors = ctx.run_function(*ctx.input_tensors)
-        return output_tensors
-
-    @staticmethod
-    def backward(ctx, *output_grads):
-        ctx.input_tensors = [x.detach().requires_grad_(True) for x in ctx.input_tensors]
-        with th.enable_grad():
-            # Fixes a bug where the first op in run_function modifies the
-            # Tensor storage in place, which is not allowed for detach()'d
-            # Tensors.
-            shallow_copies = [x.view_as(x) for x in ctx.input_tensors]
-            output_tensors = ctx.run_function(*shallow_copies)
-        input_grads = th.autograd.grad(
-            output_tensors,
-            ctx.input_tensors + ctx.input_params,
-            output_grads,
-            allow_unused=True,
-        )
-        del ctx.input_tensors
-        del ctx.input_params
-        del output_tensors
-        return (None, None) + input_grads
