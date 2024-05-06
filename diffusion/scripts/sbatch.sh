160d7750aad3d31cc7cc81f9508402dbf9f6eb93
LetianY <DawnEureka@outlook.com>
2024-05-06 04:02:04 -0400

    Change config

--- diffusion/scripts/sbatch.sh
+++ diffusion/scripts/sbatch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#SBATCH -p gpu --gres=gpu:1
 #SBATCH -n 4
-#SBATCH --mem=16G
-#SBATCH -t 1:00:00
+#SBATCH --mem=20G
+#SBATCH -t 10:00:00
 module load cudnn cuda
-source pytorch.venv/bin/activate
+source ../../pytorch.venv/bin/activate
 bash scripts/run_train.sh
